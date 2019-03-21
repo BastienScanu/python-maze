55904c0af8f394360c13bc33948384a69f3c97de
BastienScanu <bastien.scanu@gmail.com>
2019-03-21 17:58:19 +0100

    divide functions.py into two files

--- clientFunctions.py
+++ clientFunctions.py
@@ -1,45 +1,9 @@
 # -*-coding:Utf-8 -*
 
-"""Ce fichier contient les fonctions nécessaires au déroulement du jeu
+"""Ce fichier contient les fonctions utilisées par le client
 
 """
 
-import os
-import pickle
-
-from map import Map
-
-def loadMaps():
-  """ Chargement des cartes depuis le dossier maps """
-  maps = []
-  for fileName in os.listdir("maps"):
-      if fileName.endswith(".txt"):
-          chemin = os.path.join("maps", fileName)
-          with open(chemin, "r") as file:
-              content = file.read()
-              map = Map(fileName, content)
-              maps.append(map)
-  return maps
-
-def chooseMap(maps):
-  """ Choix de la carte """
-  print("Existing mazes:")
-  for i, map in enumerate(maps):
-      print("  {} - {}".format(i + 1, map.name))
-  while 1:
-    i = raw_input("Enter the number of the maze you want to play")
-    try:
-      i = int(i)
-      print maps[i-1]
-      if i <= len(maps):
-        chosenMap = maps[i - 1]
-        print "You chose the map {}:\n{}".format(chosenMap.name, chosenMap.maze)
-        return chosenMap
-      else:
-        print "The number must be between 1 and {}.".format(len(maps))
-    except:
-      print "You must enter a digit between 1 and {}.".format(len(maps))
-      
 def pickYourMove(map):
   """ Tour de jeu : on demande au joueur d'entrer sa commande """
   done = False    # True quand le tour de jeu est fini
